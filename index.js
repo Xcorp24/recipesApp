4052f8011b0c0eb61bedd4a3e9d81beddbbacb96
Denis Matejka <matejkadenis24@gmail.com>
2021-07-19 13:22:27 +0200

    vytvorene pripojenie do mysql db a vytvorene api na ziskanie dat recepty z mysql db

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Home page");
+app.get("/recipes-app/api/recipes", async (req, res) => {
+  const data = await getData("SELECT * FROM recipes");
+  res.json(data);
 });
+
+function getData(query) {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "recipesApp",
+  });
+  return new Promise(function (res, rej) {
+    connection.query(query, function (err, results, fields) {
+      if (err) rej(err);
+
+      res(results);
+    });
+  });
+}
 
 app.listen(PORT, (err) => {
   if (err) throw err;
